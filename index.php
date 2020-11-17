7014fbcd60385d2c40343610ca1517df38f28705
lucasbins <lucas.braga@rede.ulbra.br>
2020-11-17 02:21:42 -0300

    Card php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <title>Cursos WEB</title>
 
 </head>
+
 <body>
     <div>
         <header class="container-flex">
@@ -36,25 +37,43 @@
                     <a href=""><img src="assets\1.png"></a>
                 </figure>
             </div>
-            <!--- card -->
-            <div class="row">
-                <div class="col-6 col-md-4">
-                    <div class="card" style="width: 18rem; margin-top: 2rem;">
-                        <img src="assets\img1.png" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">PHP ARRAYS</h5>
-                            <p class="card-text">Esse cursos ensina a utilização de arrays.</p>
-                            <a href="#" class="btn btn-primary">Inscrever-se</a>
-                        </div>
-                    </div>
+            <!--card-->
+            <div class="card-deck">
+                <div class="row">
+                    <?php geracard(); ?>
                 </div>
             </div>
-            
-            
         </main>
         <footer class="container-fluid">
             <h5>COPYRIGHT Lucas Bins - Avaliação Semestral - Programação WEB - ULBRA 2020</h5>
         </footer>
     </div>
 </body>
+
 </html>
+
+<?php
+
+function geracard()
+{
+
+    $conn = new mysqli("localhost", "root", "", "webcursos");
+
+    $result = $conn->query("SELECT * FROM tb_cursos");
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<div">
+                    <div class="card" style="width: 18rem; margin-top: 2rem;">
+                        <img src="assets/' . $row["img"] . '" class="card-imagem" alt="..." ">
+                        <div class="card-body">
+                            <h5 class="card-title">' . $row["nomecurso"] . '</h5>
+                            <p class="card-text">' . $row["descriçao"] . '</p>
+                            <a href="#" class="btn btn-primary" style="position: absolute; bottom:2vh; background: indigo; border: none;">Inscrever-se</a>
+                        </div>
+                    </div>
+            </div>';
+    }
+}
+
+
+?>
---
+++ php/functions.php
@@ -0,0 +1,36 @@
+<?php
+	session_start();
+	if(count($_POST)!=0){
+		$dados = $_POST;
+
+		$login = $dados['login'];
+		$senha = $dados['senha'];
+
+		$conn = new mysqli("localhost", "root","","webcursos");
+
+		if($conn->connect_error){
+			echo "Error: ".$conn->connect_error;
+		}else{
+			echo 'Conectado';
+		}
+
+		$result = $conn->query("SELECT * FROM `tb_users` WHERE `login` = '$login' AND `senha`= '$senha'");
+
+		if(mysqli_num_rows ($result) > 0 ){
+			$_SESSION['login'] = $login;
+			$_SESSION['senha'] = $senha;
+			header('location: pagina.php');
+		}else{
+  			session_destroy();
+  			header('location: login.php?error=1');
+  		}
+	}
+
+	if(isset($_GET['logout'])){
+		session_unset();
+		session_destroy();
+		header('location: login.php');
+	}
+
+	
+?>
